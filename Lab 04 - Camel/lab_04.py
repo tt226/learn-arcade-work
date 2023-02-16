2fb111ee624fae4b99eb0bfc4ee1a6c7c3affb02
Tayyaba <tt262767@gmail.com>
2023-02-16 02:42:35 -0500

    test

--- Lab 04 - Camel/lab_04.py
+++ Lab 04 - Camel/lab_04.py
@@ -24,22 +24,35 @@ Q. Quit.\n""")
 
         user_choice = input("Your choice? ")
 
-        if user_choice == 'Q' or 'q':
+        if user_choice == 'Q':
             print("you have quit the game.")
             break
-
-        elif user_choice == "e" or 'E':
+        if user_choice == 'q':
+            print("you have quit the game.")
+            break
+        elif user_choice == 'E':
             print("\nTravelled " + str(miles_traveled) + " miles.")
             print("You have " + str(drinks) + " drinks left.")
             print("The natives are " + str(miles_traveled - native_travel_distance) + " miles behind you.\n")
 
-        elif user_choice == "d" or "D":
+        elif user_choice == 'e':
+            print("\nYou have now travelled " + str(miles_traveled) + " miles.")
+            print("You have " + str(drinks) + " drinks left.")
+            print("The natives are " + str(miles_traveled - native_travel_distance) + " miles behind you.\n")
+
+        elif user_choice == "D":
             native_travel_distance = native_travel_distance + random.randrange(7, 14)
             print("\nTime to rest for the night.")
             print("\U0001F42A is happy and rested.")
             print("The natives are " + str(miles_traveled - native_travel_distance) + " miles behind you.\n")
 
-        elif user_choice == "c" or 'C':
+        elif user_choice == "d":
+            native_travel_distance = native_travel_distance + random.randrange(7, 14)
+            print("\nTime to rest for the night.")
+            print("\U0001F42A is happy and rested.")
+            print("The natives are " + str(miles_traveled - native_travel_distance) + " miles behind you.\n")
+
+        elif user_choice == "C":
             miles_traveled = miles_traveled + random.randrange(10, 20)
             thirst = thirst + 1
             camel_tiredness = camel_tiredness + random.randrange(1, 3)
@@ -48,7 +61,25 @@ Q. Quit.\n""")
             print("You've travelled " + str(miles_traveled) + " miles.")
             print("The natives are " + str(miles_traveled - native_travel_distance) + " miles behind you.\n")
 
-        elif user_choice == "b" or "B":
+        elif user_choice == "c":
+            miles_traveled = miles_traveled + random.randrange(10, 20)
+            thirst = thirst + 1
+            camel_tiredness = camel_tiredness + random.randrange(1, 3)
+            native_travel_distance = native_travel_distance + random.randrange(7, 14)
+            print("\U0001F42A\U0001F4A5")
+            print("You've travelled " + str(miles_traveled) + " miles.")
+            print("The natives are " + str(miles_traveled - native_travel_distance) + " miles behind you.\n")
+
+        elif user_choice == "B":
+            miles_traveled = miles_traveled + random.randrange(5, 12)
+            thirst = thirst + 1
+            camel_tiredness = camel_tiredness + 1
+            native_travel_distance = native_travel_distance + random.randrange(7, 14)
+            print("(๑˃ᴗ˂)ﻭ")
+            print("You've travelled " + str(miles_traveled) + ' miles.')
+            print("The natives are " + str(miles_traveled - native_travel_distance) + " miles behind you.\n")
+
+        elif user_choice == "b":
             miles_traveled = miles_traveled + random.randrange(5, 12)
             thirst = thirst + 1
             camel_tiredness = camel_tiredness + 1
@@ -57,7 +88,16 @@ Q. Quit.\n""")
             print("You've travelled " + str(miles_traveled) + " miles.")
             print("The natives are " + str(miles_traveled - native_travel_distance) + " miles behind you.\n")
 
-        elif user_choice == "a" or "A":
+        elif user_choice == "A":
+            if drinks > 0:
+                thirst = 0
+                drinks = drinks - 1
+                print("╰( ^o^)╮╰( ^o^)╮")
+                print("refreshing\n")
+            else:
+                print("no more water left \U0001F494\n")
+
+        elif user_choice == "a":
             if drinks > 0:
                 thirst = 0
                 drinks = drinks - 1
@@ -93,6 +133,8 @@ Q. Quit.\n""")
         if miles_traveled >= 199:
             print("You Won \U0001F389")
             break
+
+
 
 
 main()
