4c08f14d7e7cddedad1dd608c704ed0efb6e8313
Tayyaba <tt262767@gmail.com>
2023-02-16 02:59:34 -0500

    test

--- Lab 04 - Camel/lab_04.py
+++ Lab 04 - Camel/lab_04.py
@@ -6,7 +6,8 @@ def main():
     print("""You have stolen a camel to make your way across the great Mobi desert
 The natives want their camel back and are chasing you down! Survive your 
 desert trek and out run the natives.\n""")
-    #  variable
+
+    #  variables
     done = False
     miles_traveled = 0
     thirst = 0
@@ -14,7 +15,7 @@ desert trek and out run the natives.\n""")
     native_travel_distance = -20
     drinks = 3
 
-    while done == False:
+    while not done:
         print("""\nA. Drink from your canteen.
 B. Ahead moderate speed.
 C. Ahead full speed.
@@ -24,12 +25,18 @@ Q. Quit.\n""")
 
         user_choice = input("Your choice? ")
 
+# option Q - quit the game
+
         if user_choice == 'Q':
             print("you have quit the game.")
-            break
+            done = True
+
         if user_choice == 'q':
             print("you have quit the game.")
-            break
+            done = True
+
+# option E - user status check
+
         elif user_choice == 'E':
             print("\nTravelled " + str(miles_traveled) + " miles.")
             print("You have " + str(drinks) + " drinks left.")
@@ -40,17 +47,23 @@ Q. Quit.\n""")
             print("You have " + str(drinks) + " drinks left.")
             print("The natives are " + str(miles_traveled - native_travel_distance) + " miles behind you.\n")
 
+# option D - stopping for the night
+
         elif user_choice == "D":
             native_travel_distance = native_travel_distance + random.randrange(7, 14)
+            camel_tiredness = 0
             print("\nTime to rest for the night.")
             print("\U0001F42A is happy and rested.")
             print("The natives are " + str(miles_traveled - native_travel_distance) + " miles behind you.\n")
 
         elif user_choice == "d":
             native_travel_distance = native_travel_distance + random.randrange(7, 14)
+            camel_tiredness = 0
             print("\nTime to rest for the night.")
             print("\U0001F42A is happy and rested.")
             print("The natives are " + str(miles_traveled - native_travel_distance) + " miles behind you.\n")
+
+# option C - Full power mode on
 
         elif user_choice == "C":
             miles_traveled = miles_traveled + random.randrange(10, 20)
@@ -70,6 +83,8 @@ Q. Quit.\n""")
             print("You've travelled " + str(miles_traveled) + " miles.")
             print("The natives are " + str(miles_traveled - native_travel_distance) + " miles behind you.\n")
 
+# option B - Moderate speed ahead
+
         elif user_choice == "B":
             miles_traveled = miles_traveled + random.randrange(5, 12)
             thirst = thirst + 1
@@ -87,6 +102,8 @@ Q. Quit.\n""")
             print("(๑˃ᴗ˂)ﻭ")
             print("You've travelled " + str(miles_traveled) + " miles.")
             print("The natives are " + str(miles_traveled - native_travel_distance) + " miles behind you.\n")
+
+# option A - Drinks from the canteen
 
         elif user_choice == "A":
             if drinks > 0:
@@ -106,22 +123,28 @@ Q. Quit.\n""")
             else:
                 print("no more water left \U0001F494\n")
 
+# how tired the camel is
+
         if camel_tiredness > 8:
             print("Your camel is dead ( ╥ω╥ )")
             print("\nGame Over")
-            break
+            done = True
 
         elif camel_tiredness > 5:
             print("Your camel is getting tired!")
+
+# dying of thirst
 
         if thirst > 6:
             print("\nYou died of thirst!”")
             print("¯\_(ツ)_/¯")
             print("\n Game Over")
-            break
+            done = True
 
         elif thirst > 4:
             print("You are thirsty!!! o(>< )o")
+
+# the native chase distance
 
         if native_travel_distance == miles_traveled:
             print("The natives caught you")
@@ -130,10 +153,15 @@ Q. Quit.\n""")
         elif native_travel_distance >= miles_traveled - 15:
             print("The natives are getting closer")
 
+# going over or equal to 200 miles
         if miles_traveled >= 199:
             print("You Won \U0001F389")
-            break
 
+            done = True
+
+# finding an oasis
+
+        if not done and thirst < 4:
 
 
 
