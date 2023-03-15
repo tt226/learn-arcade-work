55590860b24106cbc58a5a4964a31fb2afceb376
Tayyaba <tt262767@gmail.com>
2023-03-15 14:05:36 -0400

    test

--- Lab 07 - User Control/lab_07.py
+++ Lab 07 - User Control/lab_07.py
@@ -27,19 +27,23 @@ class Building():
         arcade.draw_rectangle_filled(self.position_x, self.position_y, self.width - 5, self.height / -5,
                                      arcade.color.PAPAYA_WHIP)
         arcade.draw_rectangle_filled(self.position_x, self.position_y, 90, 44, arcade.color.BLUEBERRY)
+        arcade.draw_text("OPEN", start_x=340, color=arcade.color.OCHRE, start_y=410, font_name="Kenney Rocket Square",
+                         font_size= 45, bold=True)
+        arcade.draw_circle_filled(130, 500, 60, arcade.color.INDIAN_YELLOW)
 
     def update(self):
         self.position_x += self.change_x
         self.position_y += self.change_y
 
 
+
 class MyGame(arcade.Window):
 
     def __init__(self):
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, "Lab 7 - User Control")
-        arcade.set_background_color(arcade.color.OCHRE)
+        arcade.set_background_color(arcade.color.BLACK)
         self.building = Building(610, 140, 210, 530, arcade.color.AMBER, 0, 0)
-        self.window = Building(310, 140, 210, 530, arcade.color.AIR_FORCE_BLUE, 0, 0)
+        self.window = Building(390, 140, 210, 530, arcade.color.AIR_FORCE_BLUE, 0, 0)
 
     def on_draw(self):
         arcade.start_render()
