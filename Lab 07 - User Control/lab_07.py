65eb3edb74298b8850d9251d1abf4d7eed8cbe84
Tayyaba <tt262767@gmail.com>
2023-03-15 16:12:32 -0400

    test

--- Lab 07 - User Control/lab_07.py
+++ Lab 07 - User Control/lab_07.py
@@ -35,6 +35,18 @@ class Sun():
             self.position_y = SCREEN_WIDTH - self.radius
 
 
+class Ring():
+
+    def __init__(self, position_x, position_y, color):
+        self.position_x = position_x
+        self.position_y = position_y
+        self.color = color
+
+    def on_draw(self):
+        arcade.draw_ellipse_outline(self.position_x, self.position_y, 100, 50, self.color, 3)
+        arcade.draw_ellipse_outline(self.position_x, self.position_y, 50, 100, arcade.color.OCHRE, 3)
+        arcade.draw_circle_filled(self.position_x, self.position_y, 13, arcade.color.PINK_SHERBET, 23)
+
 
 class Building:
 
@@ -56,27 +68,28 @@ class Building:
         arcade.draw_rectangle_filled(self.position_x, self.position_y, self.width - 5, self.height / -5,
                                      arcade.color.PAPAYA_WHIP)
         arcade.draw_rectangle_filled(self.position_x, self.position_y, 90, 44, arcade.color.BLUEBERRY)
-        arcade.draw_text("(►.◄)", start_x=340, color=arcade.color.OCHRE, start_y=410, font_name="Kenney Rocket Square",
+        arcade.draw_text("(►.◄)", start_x=320, color=arcade.color.OCHRE, start_y=410,
+                         font_name="Kenney Rocket Square",
                          font_size=45, bold=True)
         arcade.draw_text("(►.◄)", start_x=540, color=arcade.color.OCHRE, start_y=410, font_name="Kenney Rocket Square",
                          font_size=45, bold=True)
 
 
-
 class MyGame(arcade.Window):
-
     def __init__(self):
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, "Lab 7 - User Control")
         arcade.set_background_color(arcade.color.BLACK)
         self.building = Building(610, 140, 210, 530, arcade.color.AMBER, 0, 0)
         self.window = Building(390, 140, 210, 530, arcade.color.AIR_FORCE_BLUE, 0, 0)
-        self.sun = Sun(120, 450,0,0, 59, arcade.color.INDIAN_YELLOW)
+        self.sun = Sun(120, 510, 0, 0, 60, arcade.color.INDIAN_YELLOW)
+        self.ring = Ring(120, 510, arcade.color.OCHRE)
 
     def on_draw(self):
         arcade.start_render()
         self.building.draw()
         self.window.draw()
         self.sun.draw()
+        self.ring.on_draw()
 
     def update(self, delta_time):
         self.sun.update()
@@ -96,8 +109,6 @@ class MyGame(arcade.Window):
             self.sun.change_x = 0
         elif key == arcade.key.UP or key == arcade.key.DOWN:
             self.sun.change_y = 0
-
-
 
 
 def main():
