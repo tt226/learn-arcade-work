1541f6bafbd38841b6b42c27e160d191b636f31d
Tayyaba Tahir <123689975+tt226@users.noreply.github.com>
2023-04-03 14:42:54 -0400

    Update lab_08.py

--- Lab 08 - Sprites/lab_08.py
+++ Lab 08 - Sprites/lab_08.py
@@ -6,8 +6,8 @@ import random
 SPRITE_SCALING_PLAYER = 0.5
 SPRITE_SCALING_COIN = 0.4
 SPRITE_SCALING_LASER = 0.4
-COIN_COUNT = 80
-LASER_COUNT = 40
+COIN_COUNT = 55
+LASER_COUNT = 25
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
@@ -35,9 +35,6 @@ class Coin(arcade.Sprite):
 class Laser(arcade.Sprite):
     def __init__(self, filename, sprite_scaling):
         super().__init__(filename, sprite_scaling)
-
-        self.center_x += self.change_x
-        self.center_y += self.change_y
 
         if self.left < 0:
             self.change_x *= -1
@@ -80,6 +77,7 @@ class MyGame(arcade.Window):
         # player info
         self.player_sprite = None
         self.score = 0
+        self.game_over = False
 
         self.set_mouse_visible(False)
 
@@ -97,6 +95,7 @@ class MyGame(arcade.Window):
         self.player_sprite = arcade.Sprite("green_spaceship.png", SPRITE_SCALING_PLAYER)
         self.player_sprite.center_x = 50
         self.player_sprite.center_y = 50
+        # add
         self.player_list.append(self.player_sprite)
 
         # yellow gem
@@ -105,17 +104,18 @@ class MyGame(arcade.Window):
             # position the coins
             coin.center_x = random.randrange(SCREEN_WIDTH)
             coin.center_y = random.randrange(SCREEN_HEIGHT)
+            # add
             self.coin_list.append(coin)
 
         # laser
         for i in range(LASER_COUNT):
             laser = Laser("laser_beam.png", SPRITE_SCALING_LASER)
             # position the coins
-
             laser.center_x = random.randrange(120, SCREEN_WIDTH)
             laser.center_y = random.randrange(SCREEN_HEIGHT)
             laser.change_x = random.randrange(-3, 4)
             laser.change_y = random.randrange(-3, 4)
+            # add
             self.laser_list.append(laser)
 
     def on_draw(self):
@@ -125,15 +125,32 @@ class MyGame(arcade.Window):
         self.coin_list.draw()
         self.laser_list.draw()
 
+        if self.game_over:
+            arcade.draw_text("GAME OVER", start_x=140, color=arcade.color.OCHRE, start_y=350,
+                             font_name="Kenney Rocket Square",
+                             font_size=50, bold=True)
+
+            if self.score <= 50:
+                arcade.draw_text("YOU LOST \n ¯\_(ツ)_/¯", start_x=270, color=arcade.color.OCHRE, start_y=240,
+                                 font_name="Kenney Rocket Square",
+                                 font_size=20, bold=True)
+
+            if self.score == 50:
+                arcade.draw_text("YOU WON"
+                                 '°˖✧◝(⁰▿⁰)◜✧˖°', start_x=330, color=arcade.color.OCHRE, start_y=240,
+                                 font_name="Kenney Rocket Square",
+                                 font_size=20, bold=True)
+
         output = f"Score:  {self.score}"
         arcade.draw_text(output, 10, 20, arcade.color.PAPAYA_WHIP, 14)
 
     def on_mouse_motion(self, x: int, y: int, dx: int, dy: int):
+        # mouse motion
         self.player_sprite.center_x = x
         self.player_sprite.center_y = y
 
     def update(self, delta_time):
-
+        # score board
         self.coin_list.update()
         coins_hit_list = arcade.check_for_collision_with_list(self.player_sprite, self.coin_list)
         for coin in coins_hit_list:
@@ -147,6 +164,12 @@ class MyGame(arcade.Window):
             laser.remove_from_sprite_lists()
             self.score -= 1
             arcade.play_sound(lose_sound)
+
+        if len(self.coin_list) == 0:
+            self.game_over = True
+            self.coin_list.clear()
+            self.player_list.clear()
+            self.laser_list.clear()
 
 
 def main():
